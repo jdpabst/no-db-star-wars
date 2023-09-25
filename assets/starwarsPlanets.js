95c923fd937b8b7583e8786538a0982ed2fd2ae1
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2023-09-25 16:43:49 -0600

    got my faux database file set up

---
+++ assets/starwarsPlanets.js
@@ -0,0 +1,67 @@
+   [{
+	"name": "Tatooine",
+	"rotation_period": "23",
+	"orbital_period": "304",
+	"diameter": "10465",
+	"climate": "arid",
+	"gravity": "1 standard",
+	"terrain": "desert",
+	"surface_water": "1",
+	"population": "200000",
+	"imgURL" : "./images/Tattoine.png"
+   }, {
+	"name": "Alderaan",
+	"rotation_period": "24",
+	"orbital_period": "364",
+	"diameter": "12500",
+	"climate": "temperate",
+	"gravity": "1 standard",
+	"terrain": "grasslands, mountains",
+	"surface_water": "40",
+	"population": "2000000000",
+	"imgURL" : "./images/Alderaan.png"
+   }, {
+	"name": "Yavin IV",
+	"rotation_period": "24",
+	"orbital_period": "4818",
+	"diameter": "10200",
+	"climate": "temperate, tropical",
+	"gravity": "1 standard",
+	"terrain": "jungle, rainforests",
+	"surface_water": "8",
+	"population": "1000",
+	"imgURL" : "./images/Yavin IV.png"
+   }, {
+	"name": "Hoth",
+	"rotation_period": "23",
+	"orbital_period": "549",
+	"diameter": "7200",
+	"climate": "frozen",
+	"gravity": "1.1 standard",
+	"terrain": "tundra, ice caves, mountain ranges",
+	"surface_water": "100",
+	"population": "unknown",
+	"imgURL" : "./images/Hoth.png"
+   }, {
+	"name": "Dagobah",
+	"rotation_period": "23",
+	"orbital_period": "341",
+	"diameter": "8900",
+	"climate": "murky",
+	"gravity": "N/A",
+	"terrain": "swamp, jungles",
+	"surface_water": "8",
+	"population": "unknown",
+	"imgURL" : "./images/Dagobah.png"
+   }, {
+	"name": "Bespin",
+	"rotation_period": "12",
+	"orbital_period": "5110",
+	"diameter": "118000",
+	"climate": "temperate",
+	"gravity": "1.5 (surface), 1 standard (Cloud City)",
+	"terrain": "gas giant",
+	"surface_water": "0",
+	"population": "6000000",
+	"imgURL" : "./images/Bespin.png"
+   }]
